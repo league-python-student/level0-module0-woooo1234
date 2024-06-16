0810657bc5a68d3eeacff8255abe40fe4c261221
woooo1234 <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>
2024-06-15 18:00:16 -0700

    Finished all three of these checked files

--- _01_drawing/_b_tashe_me.py
+++ _01_drawing/_b_tashe_me.py
@@ -35,8 +35,8 @@ def screen_clicked(x, y):
 
     # 4. Show your moustache by calling the .showturtle() function
     # moustache.showturtle()
-    add_moustache.showturtle()
-    add_moustache.goto(13,25)
+    moustache.showturtle()
+    moustache.goto(0,5)
     # 5. Move your moustache to a new location using .goto(x, y)
 
 
@@ -48,9 +48,10 @@ if __name__ == '__main__':
     #    add the file to the folder with your code
 
     # 2. Call the set_background() function with the image filename inside of the parenthesis
-    set_background('emoji.png')
+    set_background('moustache.png')
 
     # 3. Create a variable called moustache and set it equal to add_moustache('moustache1.gif')
+    moustache = add_moustache('moustache1.gif')
     # moustache = add_moustache('moustache1.gif')
 
     # ===================== DO NOT EDIT THE CODE BELOW ============================
--- _02_code_flow/_a_turtle_spiral.py
+++ _02_code_flow/_a_turtle_spiral.py
@@ -14,21 +14,22 @@ if __name__ == '__main__':
     window.bgcolor('white')
     
     # Make a new turtle
-    
+    t = turtle.Turtle()
     # This code sets our shape to a turtle
     
     # Set your turtle's speed (0=fastest, 1=slowest, 10=faster)
-    
+    t.speed(10)
     # Set your turtle's color using .color('green')
-    
+    t.color('green')
     # Use a loop to repeat a the code below 50 times
-        
+    for i in range(50):
+        t.color(get_random_color())
         # Set the turtle color to a random color
-
+        t.color('blue')
         # Move the turtle (5*i) pixels. 'i' is the loop variable
-        
+        t.right(360/70)
         # Turn the turtle (360/7) degrees to the right
-         
+        t.width(i)
         # Change the turtle width to 'i' (the loop variable)
         
         # Check the pattern against the picture in the recipe. If it matches, you are done!
