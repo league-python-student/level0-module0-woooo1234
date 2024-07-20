5732b66bc03c7120b11e24ae473fd52c414b0c7c
woooo1234 <duggalshaan@gmail.com>
2024-07-20 16:30:24 -0700

    finished most of these.

--- _03_print_and_popups/_b_mad_libs.py
+++ _03_print_and_popups/_b_mad_libs.py
@@ -1,24 +1,26 @@
+import tkinter.simpledialog
 from tkinter import messagebox, simpledialog, Tk
 
 # Create an if-main code block, *hint, type main then ctrl+space to auto-complete
 if __name__ == '__main__':
     # Make a new window variable, window = Tk()
-
+    window = Tk()
     # Hide the window using the window's .withdraw() method
-
+    window.withdraw()
     # Put this sentence in a pop-up message box:
     # "If you find yourself having to cross a piranha-infested river, here's how to do it..."
-
+    tkinter.messagebox.showinfo('question', 'If you find yourself having to cross a piranha-infested river, here\'s how to do it...')
+    # tkinter.simpledialog.askstring('question', 'If you find yourself having to cross a piranha-infested river, here\'s how to it...')
     # Get the player to enter an adjective
-
+    tkinter.simpledialog.askstring('answer', 'Enter an adjective')
     # Get the player to enter a type of liquid
-
+    tkinter.simpledialog.askstring('answer', 'Enter a type of liquid')
     # Get the player to enter a body part
-
+    tkinter.simpledialog.askstring('answer', 'Enter a body part')
     # Get the player to enter a verb
-
+    tkinter.simpledialog.askstring('answer', 'Enter a verb')
     # Get the player to enter a place
-
+    tkinter.simpledialog.askstring('answer', 'Enter a place')
     # The story below has has been written as a group of Strings joined
     # together by + signs. The story contains place holders, indicated
     # by [** **] which you need to replace with the values entered by the
@@ -37,10 +39,15 @@ if __name__ == '__main__':
     # Make a pop-up that contains the final story. The \n escape characters add
     # line breaks to the story. If you need to, move them around to make your
     # story look better in the pop-up
+    tkinter.simpledialog.askstring('story','"Piranhas are more muddy during the day, so cross the river at\n"'
+        "night. Piranhas are attracted to fresh water and will most\n"
+        "likely take a bite out of your arm if you talk. Whatever\n"
+        "you do, if you have an open wound, try to find another way to get "
+        "back to the hospital. Good luck!"'')
 
     # If you want to write your own Madlib story, just change the story variable
     # and ask the player different questions.
 
     # Run the window's .mainloop() method
-
+    window.mainloop()
     pass
