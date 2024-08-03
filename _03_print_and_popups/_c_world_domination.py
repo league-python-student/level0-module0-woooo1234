06f2708c172fa93878cdeee7cd025d27a0ed4434
WallyWest74422 <ashaymoorjani@gmail.com>
2024-08-03 16:27:57 -0700

    tuned some stuff up

--- _03_print_and_popups/_c_world_domination.py
+++ _03_print_and_popups/_c_world_domination.py
@@ -7,13 +7,15 @@ from tkinter import messagebox, simpledialog, Tk
     # Hide the window using the window's .withdraw() method
     window.withdraw()
     # 1. Ask the user if they know how to write code.
-    name = simpledialog.askstring(title='Greeter', prompt="Do you know how to write code?")
-if name != 'yes':
+name = simpledialog.askstring(title='Greeter', prompt='Do you know how to write code?')
+if name == 'yes':
 
-    name = simpledialog.askstring(title='Greeter', prompt='Sign up for classes at The League')
+    name = simpledialog.askstring(title='Answer', prompt='You will rule the world')
 # 2. If they say "yes", tell them they will rule the world in a message box pop-up.
 
 # 3. Otherwise, tell them to sign up for classes at The League in an error box pop-up.
-    
+else:
+    name = simpledialog.askstring(title='Answer', prompt='Sign up for classes at the league.')
+
     # Run the window's .mainloop() method
     window.mainloop()
--- _04_amazing_games/_b_high_low_game.py
+++ _04_amazing_games/_b_high_low_game.py
@@ -1,3 +1,4 @@
+import tkinter
 from tkinter import messagebox, simpledialog, Tk
 import sys
 import random
@@ -10,15 +11,19 @@ if __name__ == '__main__':
     random_num = random.randint(1, 10)
 
     # 2. Print out the random variable that you made in step #1
+    print(random_num)
 
     # 3. Code a for loop to run steps 4-10, 10 times
-
+    for i in range(10):
         # 4. Ask the user for a guess using a pop-up window, and save their response
-
+    tkinter.simpledialog.askstring('answer', 'Guess a number from 1-10')
         # 5. If the guess is correct
+    if 'answer' == random_num:
+        sys.exit(0)
             # 6. Win. Use 'sys.exit(0)' to end the program
 
         # 7. if the guess is high
+
             # 8. Tell them it's too high
         # 9. Else if the guess is low
             # 10. Tell them it's too low
